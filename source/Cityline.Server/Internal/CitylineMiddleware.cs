ce68489b046dbc9d4d8fc1b38a008f0ebe26638f
Poul Foged Niesen <pf@zebrasoft.dk>
2025-12-14 10:02:34 +0100

    check after normal requests

--- source/Cityline.Server/Internal/CitylineMiddleware.cs
+++ source/Cityline.Server/Internal/CitylineMiddleware.cs
@@ -24,17 +24,17 @@ namespace Cityline.Server
         }
 
         public async Task InvokeAsync(HttpContext context, IServiceProvider serviceProvider)
-        {
-            if (!context.WebSockets.IsWebSocketRequest)
-            {
-                context.Response.StatusCode = StatusCodes.Status426UpgradeRequired;
-                return;
-            }
-
+        {   
             // Upgrade to wss seems to work better if we don't reject non-websockets here
             if (context.Request.Path != _citylineOptions.Path)
             {
                 await _next(context);
+                return;
+            }
+
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                context.Response.StatusCode = StatusCodes.Status426UpgradeRequired;
                 return;
             }
 
